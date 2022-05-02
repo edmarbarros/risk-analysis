f21989a7fa9242e24ef058bceade43fa38631e28
Edmar Barros <edmarbarros@users.noreply.github.com>
2022-05-02 20:34:22 -0300

    tests: add tests to main app

---
+++ test/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok", "info": {}, "error": {}, "details": {}}
+
